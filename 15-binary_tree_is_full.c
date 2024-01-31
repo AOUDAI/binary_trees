cb34da4dc93f01d19647238d1222818c00424637
AOUDAI <aoudair16031999@gmail.com>
2024-01-31 19:58:23 +0100

    Fix task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,26 +9,13 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL || (binary_tree_leaves(tree) % 2))
+	if (tree == NULL)
+		return (0);
+	if ((tree->left && !tree->right) || (!tree->left && tree->right))
 		return (0);
 
-	return (1);
-}
+	if (!tree->left && !tree->right)
+		return (1);
 
-/**
- * binary_tree_leaves - counts the leaves in a binary tree.
- * @tree: A pointer to the root node of the tree to count the number of leaves.
- *
- * Return: The number of leaves in the tree or 0 if tree is NULL.
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-        if (tree == NULL)
-                return (0);
-
-        if (tree->left == NULL && tree->right == NULL)
-                return (1);
-
-        return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-
+	return (binary_tree_is_full(tree->left) & binary_tree_is_full(tree->right));
 }
