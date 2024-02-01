cd63c21cba33a4b751683645faba70237ddc7775
AOUDAI <aoudair16031999@gmail.com>
2024-02-01 12:49:28 +0100

    fix task 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -11,7 +11,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	size_t height, i;
 
-	if (tree && fonc)
+	if (tree && func)
 	{
 		height = binary_tree_height(tree);
 
